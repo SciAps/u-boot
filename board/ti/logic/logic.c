4bc7da3b16181b11692a6dd8247e0e66a368499b
peterb <peterb@d7e73fac-2431-0410-abd5-fd325483733a>
2011-09-16 18:38:35 +0000

    - Changes for DM37LINUX-302:  - Add current AT88 product ID support to u-boot-2011.06.
    git-svn-id: svn+ssh://cvs.logicpd.com/cvs/eps_svn/software/products/linux/u-boot/u-boot-2011.06@20518 d7e73fac-2431-0410-abd5-fd325483733a

--- board/ti/logic/logic.c
+++ board/ti/logic/logic.c
@@ -39,6 +39,7 @@
 #include <i2c.h>
 #include <asm/mach-types.h>
 #include "logic.h"
+#include "product_id.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -144,6 +145,17 @@ int board_init(void)
 
 int board_late_init(void)
 {
+	unsigned char enetaddr[6];
+
+	dump_production_data();
+
+	/* Fetch the ethaddr of the LAN */
+	board_get_nth_enetaddr(enetaddr, 0, 0);
+#ifdef CONFIG_HAS_ETH1
+	/* Fetch the ethaddr of the WiFi */
+	board_get_nth_enetaddr(enetaddr, 1, 1);
+#endif
+
 #ifdef CONFIG_CMD_NAND_LOCK_UNLOCK
 	nand_unlock(&nand_info[0], 0x0, nand_info[0].size);
 #endif
@@ -153,6 +165,37 @@ int board_late_init(void)
 		dcache_status() ? "ON" : "OFF");
 #endif
 	return 0;
+}
+
+/* Turn on VAUX1 voltage to 3.0 volts to drive level shifters and
+ * power 3.0v parts (tsc2004 and Product ID chip) */
+#define I2C_TRITON2 0x4b /* Address of Triton power group */
+
+void init_vaux1_voltage(void)
+{
+#ifdef CONFIG_DRIVER_OMAP34XX_I2C
+	unsigned char data;
+	unsigned short msg;
+
+	i2c_init(CONFIG_SYS_I2C_SPEED, CONFIG_SYS_I2C_SLAVE);
+
+	/* Select the output voltage */
+	data = 0x04;
+	i2c_write(I2C_TRITON2, 0x72, 1, &data, 1);
+	/* Select the Processor resource group */
+	data = 0x20;
+	i2c_write(I2C_TRITON2, 0x72, 1, &data, 1);
+	/* Enable I2C access to the Power bus */
+	data = 0x02;
+	i2c_write(I2C_TRITON2, 0x4a, 1, &data, 1);
+	/* Send message MSB */
+	msg = (1<<13) | (1<<4) | (0xd<<0); /* group(process_grp1):resource(vaux1):res_active; */
+	data = msg >> 8;
+	i2c_write(I2C_TRITON2, 0x4b, 1, &data, 1);
+	/* Send message LSB */
+	data = msg & 0xff;
+	i2c_write(I2C_TRITON2, 0x4c, 1, &data, 1);
+#endif
 }
 
 /*
@@ -185,6 +228,12 @@ int misc_init_r(void)
 #ifdef CONFIG_DRIVER_OMAP34XX_I2C
 	i2c_init(CONFIG_SYS_I2C_SPEED, CONFIG_SYS_I2C_SLAVE);
 #endif
+
+	/* Turn on vaux1 to make sure voltage is to the product ID chip.
+	 * Extract production data from ID chip, used to selectively
+	 * initialize portions of the system */
+	init_vaux1_voltage();
+	fetch_production_data();
 
 #if defined(CONFIG_CMD_NET)
 	setup_net_chip();
